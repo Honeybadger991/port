6159fa730b165dc6c3a9b4af49059b39b2dfbe7b
Sanya <sasham991@ukr.net>
2023-01-08 22:46:10 +0200

    done

---
+++ src/js/script.js
@@ -0,0 +1,23 @@
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      closeElem = document.querySelector('.menu__close');
+      closeElem2 = document.querySelector('.menu__overlay');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+closeElem2.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+const counters = document.querySelectorAll('.skills__progress-name span'),
+      lines = document.querySelectorAll('.skills__progress-line span');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
